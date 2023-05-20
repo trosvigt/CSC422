ad925e6ad04ddf9c4ccb0968c441582eb89fcde3
Tristan <ttrosvig804@gmail.com>
2023-05-20 11:18:17 -0500

    Created basic Database class

---
+++ PetDatabase/src/main/java/edu/csp/petdatabase/Database.java
@@ -0,0 +1,18 @@
+package edu.csp.petdatabase;
+
+import java.util.ArrayList;
+
+/*
+This class saves and loads pets to/from pets.txt
+*/
+public class Database {
+    // This method will save pets to pets.txt
+    public static void save(ArrayList<Pet> pets) {
+        
+    }
+    
+    // This method will read in pets from pets.txt
+    public static ArrayList<Pet> load() {
+        return new ArrayList();
+    }
+}
